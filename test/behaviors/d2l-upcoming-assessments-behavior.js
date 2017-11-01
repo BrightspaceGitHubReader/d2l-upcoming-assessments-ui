f1b4c1301a808fb8b127a4a609c186f1f3312b99
Dave Harris <harris.dave@gmail.com>
2017-10-31 19:41:04 -0500

    Fail gracefully for failed activity requests (#74)

--- test/behaviors/d2l-upcoming-assessments-behavior.js
+++ test/behaviors/d2l-upcoming-assessments-behavior.js
@@ -270,6 +270,39 @@ describe('d2l upcoming assessments behavior', function() {
 
 			expect(component._fetchEntity).to.have.been.called;
 		});
+
+		it('should resolve null when the request fails with 4xx', function() {
+			var usage = getUserActivityUsage('assignment');
+			component._fetchEntity = sandbox.stub().returns(Promise.reject(404));
+
+			return component._getActivityRequest(usage, getToken, userUrl)
+				.then(function(result) {
+					expect(result).to.be.null;
+				});
+		});
+
+		it('should resolve null when the request fails with 4xx', function() {
+			var usage = getUserActivityUsage('assignment');
+			component._fetchEntity = sandbox.stub().returns(Promise.reject({ status: 400 }));
+
+			return component._getActivityRequest(usage, getToken, userUrl)
+				.then(function(result) {
+					expect(result).to.be.null;
+				});
+		});
+
+		it('should reject when the request fails with server error', function() {
+			var usage = getUserActivityUsage('assignment');
+			component._fetchEntity = sandbox.stub().returns(Promise.reject(500));
+
+			return component._getActivityRequest(usage, getToken, userUrl)
+				.then(function() {
+					return Promise.reject();
+				})
+				.catch(function(err) {
+					expect(err).to.equal(500);
+				});
+		});
 	});
 
 	describe('_getUserActivityUsagesInfos', function() {
@@ -321,6 +354,33 @@ describe('d2l upcoming assessments behavior', function() {
 				expect(component._getAssignmentInstructions).not.to.be.called;
 				expect(component._getQuizDescription).to.be.called;
 				expect(response[0].info).to.equal('time for new flava in ya ear');
+			});
+		});
+
+		it('should fail when all the activity requests fail', function() {
+			component._getActivityRequest = sandbox.stub().returns(Promise.resolve(null));
+
+			return component._getUserActivityUsagesInfos(userUsages, overdueUserUsages, getToken, userUrl)
+			.then(function() {
+				return Promise.reject('Expect failure');
+			})
+			.catch(function() {
+				return;
+			});
+		});
+
+		it('should not fail when some of the activity requests fail', function() {
+			component._getActivityRequest.onSecondCall().returns(Promise.resolve(null));
+			component._getAssignmentInstructions = sandbox.stub().returns('bonita bonita bonita');
+			component._getQuizDescription = sandbox.stub().returns('time for new flava in ya ear');
+
+			userUsages = parse({ entities: [userUsage, userUsage] });
+
+			return component._getUserActivityUsagesInfos(userUsages, overdueUserUsages, getToken, userUrl)
+			.then(function(response) {
+				expect(component._getAssignmentInstructions).to.be.called;
+				expect(component._getQuizDescription).not.to.be.called;
+				expect(response[0].info).to.equal('bonita bonita bonita');
 			});
 		});
 
